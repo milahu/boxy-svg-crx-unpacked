e9a6c45bccd45287b6eb7ee30b558f6042abe2e1
boxy-svg <>
2016-05-21 00:00:00 +0000

    version 2.9.1

--- frontend/runtime-dom.js
+++ frontend/runtime-dom.js
@@ -602,7 +602,8 @@ if (!SVGPathElement.prototype.getPathData || !SVGPathElement.prototype.setPathDa
       _parseNumber: function() {
         var exponent = 0;
         var integer = 0;
-        var decimals= [];
+        var frac = 1;
+        var decimal = 0;
         var sign = 1;
         var expsign = 1;
         var startIndex = this._currentIndex;
@@ -669,7 +670,8 @@ if (!SVGPathElement.prototype.getPathData || !SVGPathElement.prototype.setPathDa
             this._string[this._currentIndex] >= "0" &&
             this._string[this._currentIndex] <= "9"
           ) {
-            decimals.push(this._string[this._currentIndex] - "0");
+            frac *= 10;
+            decimal += (this._string.charAt(this._currentIndex) - "0") / frac;
             this._currentIndex += 1;
           }
         }
@@ -712,7 +714,7 @@ if (!SVGPathElement.prototype.getPathData || !SVGPathElement.prototype.setPathDa
           }
         }
 
-        var number = integer + parseFloat("0." + decimals.join(""));
+        var number = integer + decimal;
         number *= sign;
 
         if (exponent) {
@@ -1380,7 +1382,9 @@ if (!SVGPathElement.prototype.getPathData || !SVGPathElement.prototype.setPathDa
             pathData = clonePathData(this[symbols.cachedPathData]);
           }
           else {
+            console.time("parsePathDataString");
             pathData = parsePathDataString(this.getAttribute("d") || "");
+            console.timeEnd("parsePathDataString");
             this[symbols.cachedPathData] = clonePathData(pathData);
           }
 
